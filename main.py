422adbff108882b769974459371dab356a7a185c
Luke Busch <luke.busch@gmail.com>
2025-04-16 19:43:21 -0500

    added new parser, now getting proper page elements via Beautiful Soup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from bs4.diagnose import diagnose
 
 # practice URL
 practice_url = "https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub"
@@ -9,12 +10,14 @@ evaluation_url =  "https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrB
 
 def get_data(url):
     raw_data = requests.get(url)
-    soup = BeautifulSoup(raw_data.content, "html.parser" )
-    print(soup.prettify())
+    soup = BeautifulSoup(raw_data.text, "html5lib" )
 
-    stuff_i_want = soup.find_all("tr", {"class": "c4"})
+    stuff_i_want = soup.find_all("span",
+                                 {"class":"c1"}
+                                 )
     print("This is all of the selected tags:")
     print(stuff_i_want)
+
 
 if __name__ == '__main__':
     get_data(practice_url)
